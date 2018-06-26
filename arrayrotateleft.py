7e1a8cee8beb1acbc3bd3a864e2ddb6f76cd09d0
Varun Agarwal <varun1997agarwal@gmail.com>
2018-06-26 20:22:30 +0530

    Update arrayrotateleft.py

--- arrayrotateleft.py
+++ arrayrotateleft.py
@@ -3,11 +3,11 @@ arr = [1,2,3,4,5]
 
 def arr_rot(arr, n, rot):
     for i in range(rot):
-        temp = arr[n-1] 
-        for i in range(n-1, 0, -1):
-            arr[i] = arr[i-1]
+        temp = arr[0]
+        for i in range(0, n-1):
+            arr[i] = arr[i+1]
 
-        arr[0] = temp
+        arr[n-1] = temp
         print(arr)
 
 arr_rot(arr, 5, 1)
